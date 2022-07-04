9b598d295451d270833c348b4a5c164d28ad60d0
Semih Arslan <semihsemih@me.com>
2022-07-04 18:50:24 +0300

    added action buttons

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, View, Image } from "react-native";
 
 function ProductItem(props) {
   return (
     <View style={styles.itemContainer}>
-      <Text>{props.item.name}</Text>
-      <View style={styles.quantityContainer}>
-        <Text>{props.item.quantity}</Text>
-        <Text>{props.item.measure.name}</Text>
+      <View style={styles.itemDetailContainer}>
+        <Text style={styles.itemDetailText}>{props.item.name}</Text>
+        <Text style={styles.itemDetailText}>
+          {props.item.quantity} {props.item.measure.name}
+        </Text>
+        <Text style={styles.itemDetailText}>{props.item.location}</Text>
       </View>
-      <Text>{props.item.location}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.actionButton}>
+          <Image
+            style={styles.actionButtonImage}
+            source={require("../assets/images/delete.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.actionButton}>
+          <Image
+            style={styles.actionButtonImage}
+            source={require("../assets/images/edit.png")}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -17,11 +32,33 @@ const styles = StyleSheet.create({
   itemContainer: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
+    backgroundColor: "green",
+    margin: 4,
+    paddingLeft: 4,
   },
-  quantityContainer: {
+  itemDetailContainer: {
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemDetailText: {
+    color: "white",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 8,
+    padding: 8,
+    backgroundColor: "red",
+  },
+  actionButton: {
+    padding: 4,
+  },
+  actionButtonImage: {
+    width: 20,
+    height: 20,
   },
 });
 
